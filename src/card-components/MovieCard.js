481067c19c8a11dad0c7fff4252b4776ea8a30cb
2daysweb <sahnunhm@gmail.com>
2019-07-15 01:48:42 -0400

    Done.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -32,12 +32,38 @@ export default class MovieCard extends Component {
   render() {
     return (
       <div className="movie-card">
-        {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        {/* which component should receive whichreturn 
+        <MovieCard 
+    key = {movie.title} 
+    title = {movie.title}
+    IMDbRating = {movie.IMDBRating}
+    genres = {movie.genres}
+    poster = {movie.poster} /> props? */}
+        <CardFront 
+       poster = {this.props.poster} 
+         />
+        <CardBack 
+        title = {this.props.title}
+        IMDBRating = {this.props.IMDBRating}
+        genres = {this.props.genres}
+        />
       </div>
     )
   }
 }
 
-// Don't forget your default props!
+MovieCard.defaultProps = {
+ title: 'Unknown',
+ IMDBRating: null, 
+ genres: ["No Genre(s) Found"],
+ poster: "default"
+}
+
+
+// - `title` receives an 'Unknown' string
+// - `IMDBRating` simply gets assigned to null
+// - `genres` should receive a value that will work with our `CardBack` component's rendering method for genres. The screen should read: 'No Genre(s) Found'
+// - `poster` should get the string `default`
+
+// 2.  pass the correct props to the correct back/front components
+//     (**Note:** the `posterMap` already takes care of converting a string into the appropriate poster asset)
